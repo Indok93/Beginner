75c848681b6e78e5ce0907fe9f3202262c779161
Indok93 <55687559+Indok93@users.noreply.github.com>
2022-08-18 11:36:55 +0200

    gallerycard aflevering

--- assets/js/myApp.js
+++ assets/js/myApp.js
@@ -1,9 +1,32 @@
+// app element required
+let imageOnCard = "https://www.dyrenesbeskyttelse.dk/sites/dyrenesbeskyttelse.dk/files/2020-02/Roskilde%20sort%20kat%20KIM%20JOHNSEN.jpg";
+let imageHeader = "Venlig kattekilling?";
+let imageDescription = "Dette er Kiwi, hun er 2 måneder gammel og MEGET skræmmende"
+let imageWidth = 500;
+let imageHeight = 300;
+
+
 const myApp = document.getElementById("app");
 const newDiv = document.createElement("div");
 
-console.log(newDiv);
-console.log(myApp);
+const myCard = document.createElement("figure");
+const cardHeadline = document.createElement("h2");
+const cardDescription = document.createElement("p")
 
+// definere text
 newDiv.innerHTML = "<h1>hellow world</h1>"
+cardHeadline.innerText = imageHeader;
+cardDescription.innerHTML = imageDescription;
 
-myApp.appendChild(newDiv);
+// Billede
+const cardImage = document.createElement("img");
+cardImage.src = imageOnCard;
+cardImage.setAttribute("height", imageHeight);
+cardImage.setAttribute("width", imageWidth);
+
+//indsætter elementer i DOM
+// myApp.appendChild(newDiv);
+myApp.appendChild(myCard);
+myCard.appendChild(cardHeadline);
+myCard.appendChild(cardImage);
+myCard.appendChild(cardDescription);
